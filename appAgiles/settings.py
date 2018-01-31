ab57727a7a935abf71efe77dc639ded7aede678a
Julian <jlian92@hotmail.com>
2018-01-30 21:41:02 -0500

    Se agrega conexion a postgresql

--- appAgiles/settings.py
+++ appAgiles/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'appAgiles.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'promotions',
+        'USER': 'postgres',
+        'PASSWORD': 'ironman123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
