ee5c7ca5c238224e941f72b7fc767f986a0003e0
Sergio Perez <sperez@hugeinc.com>
2018-02-03 11:10:39 -0500

    reqs

--- appAgiles/settings.py
+++ appAgiles/settings.py
@@ -129,3 +129,15 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
