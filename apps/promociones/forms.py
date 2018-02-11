a6d2713071304bdb111281c0df9a37cd54a99500
Sergio Perez <se.perezf@uniandes.edu.co>
2018-02-11 02:28:58 -0500

    Adicionar link y usar template de admin

--- apps/promociones/forms.py
+++ apps/promociones/forms.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User as AuthUser
 class AuthUserForm(forms.ModelForm):
     class Meta:
         model = AuthUser
-        fields = ('first_name', 'last_name', 'email')
+        fields = ['first_name', 'last_name', 'email']
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('photo', 'country', 'city', 'address')
+        fields = ['photo', 'country', 'city', 'address']
--- apps/promociones/views.py
+++ apps/promociones/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import *
+from .forms import AuthUserForm, UserForm
+from .models import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db import transaction
@@ -23,12 +24,13 @@ def update_profile(request):
         if auth_form.is_valid() and user_form.is_valid():
             auth_form.save()
             user_form.save()
-            messages.success(request, _('Usuario actualizado!'))
-            return redirect('')
+            messages.success(request, 'Usuario actualizado!')
+            return redirect('/admin')
         else:
-            messages.error(request, _('Corrija el error..'))
+            messages.error(request, 'Corrija el error..')
     else:
         auth_form = AuthUserForm(instance=request.user)
+        print(request.user.user.city)
         user_form = UserForm(instance=request.user.user)
     return render(request, 'user/user.html', {
         'auth_form': auth_form,
