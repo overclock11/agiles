d6151a3d979dbe614016f097acf4b0aaa59a3217
jramell <sergiomaderarangel@gmail.com>
2018-02-11 00:31:19 -0500

    login funcionando, falta sesión de usuario global

--- apps/promociones/urls.py
+++ apps/promociones/urls.py
@@ -5,4 +5,6 @@ from apps.promociones.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('<promotion_id>', promotionDetails, name='promotion_details'),
+    path('login/signin', loginPage, name='login_page'),
+    path('login/request', loginRequest, name='login_request')
 ]
--- apps/promociones/views.py
+++ apps/promociones/views.py
@@ -9,4 +9,23 @@ def index(request):
 
 def promotionDetails(request, promotion_id):
     promotion = Promotion.objects.get(id= promotion_id)
-    return render(request,'promociones/promotionDetails.html',{'promotion':promotion})    
+    return render(request,'promociones/promotionDetails.html',{'promotion':promotion})
+
+def loginPage(request):
+    return render(request, 'promociones/login.html')
+
+def loginRequest(request):
+    try:
+        user = User.objects.get(user=request.POST['username'])
+    except User.DoesNotExist:
+        context = { "error_msg": "Invalid user, please try again" }
+        return render(request, 'promociones/login.html', context)
+
+    if(user.passw == request.POST['password']):
+        context = { "user" : user }
+    else:
+        context = {"error_msg": "Invalid password, please try again"}
+        return render(request, 'promociones/login.html', context)
+
+    promotions = Promotion.objects.values() #valid login
+    return render(request, 'promociones/index.html', {'promotions': promotions})
