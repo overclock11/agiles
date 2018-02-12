4f328105cf12451af85e1325e84b0f4d2fe54694
jramell <sergiomaderarangel@gmail.com>
2018-02-11 21:27:46 -0500

    left over code and templates removed

--- apps/promociones/views.py
+++ apps/promociones/views.py
@@ -12,29 +12,7 @@ def index(request):
 
 def promotionDetails(request, promotion_id):
     promotion = Promotion.objects.get(id= promotion_id)
-    return render(request,'promociones/promotionDetails.html',{'promotion':promotion})
-
-def loginPage(request):
-    return render(request, 'promociones/login.html')
-
-def loginRequest(request):
-    try:
-        user = User.objects.get(user=request.POST['username'])
-    except User.DoesNotExist:
-        context = { "error_msg": "Invalid user, please try again" }
-        return render(request, 'promociones/login.html', context)
-
-    if(user.passw != request.POST['password']):
-        context = {"error_msg": "Invalid password, please try again"}
-        return render(request, 'promociones/login.html', context)
-
-    # On successfull login...
-    request.session["loggedUser"] = user.to_dictionary()
-    return index(request)
-
-def logoutRequest(request):
-    request.session["loggedUser"] = None
-    return index(request)
+    return render(request,'promociones/promotionDetails.html',{'promotion': promotion})
 
 @login_required
 @transaction.atomic
